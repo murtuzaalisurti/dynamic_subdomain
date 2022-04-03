6b101b292bc399b527fdbe7a0929ab4c05a53d67
murtuza <murtuza.creativity@gmail.com>
2022-04-03 12:58:23 +0530

    cleaned & added bootstrap ✔

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import {Button, Badge, Anchor} from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React JS
-        </a>
-      </header>
-    </div>
+    <>
+      <Button>hello</Button>
+      <Badge>hello</Badge>
+      <Anchor href="/">hello</Anchor>
+    </>
   );
 }
 
